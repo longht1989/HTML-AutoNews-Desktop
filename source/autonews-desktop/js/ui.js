2411f8dd16eef5c5f9af3e5f5e2bf3486de5788c
longht1989 <thanhlong.vn09@gmail.com>
2019-07-08 10:41:02 +0700

    update plugin zoom in detail article

--- source/autonews-desktop/js/ui.js
+++ source/autonews-desktop/js/ui.js
@@ -1,52 +1,52 @@
 /*
-* TABLE OF CONTENTS
-*
-* bx-slider
-* go top
-* photoswipe
-* toolsPage
-* channel-timeline
-*    -> toggle login-board
-*    -> toggle setting-board
-* report choose tag
-*    -> toggle context-menu channelpage
-*/
+ * TABLE OF CONTENTS
+ *
+ * bx-slider
+ * go top
+ * photoswipe
+ * toolsPage
+ * channel-timeline
+ *    -> toggle login-board
+ *    -> toggle setting-board
+ * report choose tag
+ *    -> toggle context-menu channelpage
+ */
 
 
 // > bx-slider
 $('.hero-zone .wrap').bxSlider({
     minSlides: 1,
     maxSlides: 1,
-    slideMargin : 0,
+    slideMargin: 0,
     controls: false,
     // nextSelector: ".most-read-box .btn-next",
     // prevSelector: ".most-read-box .btn-prev",
     // auto : true,
-    pause : 20000
+    pause: 20000
 });
 
 $('.best-selling-car .wrap').bxSlider({
     slideWidth: 400,
     minSlides: 1,
     maxSlides: 1,
-    slideMargin : 20,
+    slideMargin: 20,
     controls: false,
     // nextSelector: "#sidebar-video .btn-next",
     // prevSelector: "#sidebar-video .btn-prev",
     // auto : true,
-    pause : 20000
+    pause: 20000
 });
 
 $('.most-read-box .wrap').bxSlider({
     slideWidth: 230,
     minSlides: 3,
     maxSlides: 4,
-    slideMargin : 0,
+    slideMargin: 0,
     // controls: false,
     nextSelector: ".most-read-box .btn-next",
     prevSelector: ".most-read-box .btn-prev",
     // auto : true,
-    pause : 20000
+    pause: 20000
 });
 
 // > tab
@@ -66,3 +66,7 @@ $(".f1-table .panel > button").click(function(e) {
     $(this).siblings('.f1-table-detail').toggleClass("is-active");
 });
 
+mediumZoom('[data-zoomable]', {
+    margin: 24,
+    background: '#272727'
+});
